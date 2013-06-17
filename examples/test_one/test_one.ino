f1f915a282f69a1753d64f8be7c35936189dbbce
loovee <luweicong@seeedstudio.com>
2013-06-17 16:23:00 +0800

    add examples and ...

---
+++ examples/test_one/test_one.ino
@@ -0,0 +1,20 @@
+// demo of xadow, test one
+#include <Streaming.h>
+
+#include "xadow.h"
+#include "xadowDfs.h"
+
+void setup()
+{
+    Serial.begin(115200);
+    cout << "hello world" << endl;
+}
+
+void loop()
+{
+     Xadow.init();
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
---
+++ xadow.cpp
@@ -0,0 +1,39 @@
+/*
+  xadow.cpp
+  2013 Copyright (c) Seeed Technology Inc.  All right reserved.
+
+  Author:Loovee
+  2013-6-17
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "xadowDfs.h"
+#include "xadow.h"
+
+/*********************************************************************************************************
+** Function name:           init
+** Descriptions:            init
+*********************************************************************************************************/
+void xadow::init()
+{
+    
+}
+
+xadow Xadow;
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
