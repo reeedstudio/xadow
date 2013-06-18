be9f18eeba42608f666b5b54b509ae0a16c3934c
reeedstudio <luweicong@qq.com>
2013-06-18 15:26:26 +0800

    add acc example

---
+++ examples/accelermeter/accelermeter.ino
@@ -0,0 +1,40 @@
+// demo of xadow, test one
+#include <Streaming.h>
+#include <Wire.h>
+
+#include "xadow.h"
+#include "xadowDfs.h"
+
+void setup()
+{
+    Serial.begin(115200);
+    
+    while(!Serial);
+    
+    delay(2000);
+    cout << "hello world" << endl;
+    Xadow.init();
+    
+}
+
+double ax, ay, az;
+void loop()
+{
+
+    int i=1;
+    
+    cout << "hello" << endl;
+    cout << ++i + i++ << endl;
+    
+    
+    Xadow.readAcc(&ax, &ay, &az);
+    
+    cout << "ax = " << ax << '\t';
+    cout << "ay = " << ay << '\t';
+    cout << "az = " << az << endl;
+    delay(100);
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
