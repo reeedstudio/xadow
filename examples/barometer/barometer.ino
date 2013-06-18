6a32b91533b2544dc97c7805fb0d68a404da6fe5
reeedstudio <luweicong@qq.com>
2013-06-18 14:20:32 +0800

    add barometer example

---
+++ examples/barometer/barometer.ino
@@ -0,0 +1,28 @@
+// demo of xadow, test one
+#include <Streaming.h>
+#include <Wire.h>
+
+#include "xadow.h"
+#include "xadowDfs.h"
+
+void setup()
+{
+    Serial.begin(115200);
+    
+    while(!Serial);
+    
+    delay(2000);
+    cout << "hello world" << endl;
+    Xadow.init();
+    
+}
+
+void loop()
+{
+    cout << (float)Xadow.getBaro()/1000.0 << " kPa" << endl;
+    delay(500);
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
