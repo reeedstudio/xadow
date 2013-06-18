344f111b5822b115fdfd67eeaed13917c77a507b
reeedstudio <luweicong@qq.com>
2013-06-18 17:25:08 +0800

    format

--- xadowDfs.h
+++ xadowDfs.h
@@ -35,16 +35,15 @@
 #define ADDRACC         0x53
 #define ADDRRTC         0x68
 
-#if EN_ACC
-#define Register_ID 0
-#define Register_2D 0x2D
-#define Register_X0 0x32
-#define Register_X1 0x33
-#define Register_Y0 0x34
-#define Register_Y1 0x35
-#define Register_Z0 0x36
-#define Register_Z1 0x37
-#endif
+// accel...
+#define Register_ID     0
+#define Register_2D     0x2D
+#define Register_X0     0x32
+#define Register_X1     0x33
+#define Register_Y0     0x34
+#define Register_Y1     0x35
+#define Register_Z0     0x36
+#define Register_Z1     0x37
 
 // rtc
 #define MON 1
